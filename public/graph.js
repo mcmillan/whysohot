51b0669b5e93a9a62d99a9906ea5e3983daaeb67
Jesse the Game <jesse@jessethegame.net>
2015-05-05 20:15:29 +0100

    Update graph

--- public/graph.js
+++ public/graph.js
@@ -1,10 +1,13 @@
 (function (window, d3) {
   var Graph = window.Graph = {};
 
-  var minTemp = 10,
-      maxTemp = 30,
-      paths = {},
-      n, data, width, height, x, y, line, svg, now, area;
+  var timeScale = d3.time.second;
+  var interval = 200;
+  var minTemp = 0;
+  var maxTemp = 40;
+  var paths = {};
+  var axes = {};
+  var n, data, width, height, x, y, line, svg, area;
 
   function draw() {
     d3.select("svg").remove();
@@ -13,12 +16,16 @@
     height = window.innerHeight;
 
     svg = d3.select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g");
 
-    updateXAxis();
+    var now = new Date();
+
+    x = d3.scale.linear()
+      .domain([timeScale.offset(now, -n), timeScale.offset(now, -1)])
+      .range([0, width]);
 
     y = d3.scale.linear()
         .domain([minTemp, maxTemp])
@@ -30,34 +37,34 @@
       .y1(function(d) { return y(d.temperature); });
 
     line = d3.svg.line()
-        .x(function(d, i) { return x(d.timestamp); })
-        .y(function(d, i) { return y(d.temperature); });
+        .x(function(d) { return x(d.timestamp); })
+        .y(function(d) { return y(d.temperature); });
 
-    svg.append("defs").append("clipPath")
+    svg.append("defs")
+      .append("clipPath")
         .attr("id", "clip")
       .append("rect")
         .attr("width", width)
         .attr("height", height);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        // .attr("transform", "translate(0," + y(minTemp) + ")")
-        .call(d3.svg.axis()
-            .scale(x)
-            // .orient("top")
-            .orient("bottom")
-            .ticks(d3.time.minute, 5)
-            .tickFormat(d3.time.format("%H:%M")));
-            // .tickFormat(function (d) { return d3.time.format("%H:%M")(d); }));
-            // .tickFormat(function (d) { return "-0:" + d3.format("02d")(30 - d); }));
-            // .tickFormat(function (d) { return 30 - d + " minutes ago"}));
+    axes.x = d3.svg.axis()
+      .scale(x)
+      .orient("top")
+      .tickFormat(function (d) { return d3.time.format("%H:%M")(new Date(d)); });
+
+    paths.xAxis = svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + y(minTemp) + ")")
+      .call(axes.x);
+
+    axes.y = d3.svg.axis()
+      .scale(y)
+      .orient("right")
+      .tickFormat(function (d) { return d + "°C"; });
 
     svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.svg.axis()
-          .scale(y)
-          .orient("right")
-          .tickFormat(function (d) { return d + "°C"; }));
+      .attr("class", "y axis")
+      .call(axes.y);
 
     paths.line = svg.append("g")
         .attr("clip-path", "url(#clip)")
@@ -74,35 +81,45 @@
 
   function drawLine() {
     // redraw the line, and slide it to the left
-    var offset = x(d3.time.minute.offset(new Date, -1));
+    var now = new Date();
+    var offset = x(now) - x(timeScale.offset(now, 1));
 
-    // paths.line
-    //   .attr("d", line)
+    paths.line
+      .attr("d", line)
+      // .attr("transform", null)
+      .attr("transform", "translate(" + offset + ",0)")
+      .transition()
+        .duration(interval)
+        .ease("linear")
+        .attr("transform", "translate(" + offset + ",0)")
+        .each("end", function () {});
+
+    paths.area
+      .attr("d", area)
+      // .attr("transform", null)
+      .transition()
+        .duration(interval)
+        .ease("linear")
+        .attr("transform", "translate(" + offset + ",0)")
+        .each("end", function () {});
+
+    // d3.select('x axis')
     //   .attr("transform", null)
     //   .transition()
-    //     .duration(59000)
+    //     .duration(800)
     //     .ease("linear")
-    //     .attr("transform", "translate(" + offset + ",0)");
-    //     // .each("end", function () {});
-
-    // paths.area
-    //   .attr("d", area)
-    //   .attr("transform", null)
-    //   .transition()
-    //     .duration(59000)
-    //     .ease("linear")
-    //     .attr("transform", "translate(" + offset + ",0)");
-    //     // .each("end", function () {});
+    //     .attr("transform", "translate(" + offset + ", " + y(maxTemp) + ")")
+    //     .each("end", function () {})
+    //     .call(xAxis)
   }
 
-
-  function updateXAxis() {
-    now = new Date();
-    x = d3.scale.linear()
-      // .domain([0, n - 1])
-      .domain([d3.time.hour.offset(now, -1), now])
-      .range([0, width]);
-  }
+  // function updateXAxis() {
+  //   now = new Date();
+  //   x = d3.scale.linear()
+  //     // .domain([0, n - 1])
+  //     .domain([d3.time.minute.offset(now, -n), now])
+  //     .range([0, width]);
+  // }
 
   function convertDatum(raw) {
     return {temperature: raw.temperature, timestamp: new Date(raw.taken_at)};
@@ -110,18 +127,22 @@
 
   Graph.init = function (_data) {
     data = _data.map(convertDatum);
-    n = _data.length;
+    n = _data.length - 1;
     draw();
   };
 
   Graph.push = function (raw) {
     var datum = convertDatum(raw);
-    // push a new data point onto the back
     data.push(datum);
     drawLine();
-    // pop the old data point off the front
-    data.shift();
+    setTimeout(data.shift.bind(data), interval + 100);
   };
 
   window.onresize = draw;
+
+  requestAnimationFrame(function frame() {
+    draw();
+    requestAnimationFrame(frame);
+  });
+
 })(window, d3);
